7ade8afcd6f5d0a53187a1603eb57b33188253fc
sirWojciechVojas <wojciech_vojas@o2.pl>
2025-12-21 13:22:32 +0100

    Feat: Restoration frontend config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 
-const fs = require('fs');
-const path = require('path');
-
 module.exports = defineConfig({
   transpileDependencies: true,
 
   devServer: {
     port: 8080,
-    allowedHosts: 'all',
+    allowedHosts: "all",
     client: {
-      webSocketURL: 'auto://localhost/ws', // Vue połączy się przez Nginx
-    }
-  }
+      webSocketURL: "auto://localhost/ws", // Vue połączy się przez Nginx
+    },
+  },
 });
