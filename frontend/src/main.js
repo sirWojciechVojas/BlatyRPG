1891394a0a3f967fff887d8b76d96c832946b6f2
sirWojciechVojas <wojciech_vojas@o2.pl>
2026-01-19 02:04:42 +0100

    Feat: Implement bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import i18n, { initI18n } from "./i18n";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 async function initializeApp() {
